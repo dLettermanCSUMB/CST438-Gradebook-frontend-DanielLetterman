e936bd412baa771f9bc798c727b977b79bfcd35d
David <dwisneski@csumb.edu>
2021-07-25 19:52:01 -0700

    initial commit

--- src/components/Gradebook.js
+++ src/components/Gradebook.js
@@ -1,26 +1,21 @@
-import React  from 'react';
+import React, { Component } from 'react';
+import {SERVER_URL} from '../constants.js'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Grid from '@mui/material/Grid';
-import {DataGrid} from '@mui/x-data-grid';
-import Button from '@mui/material/Button';
+import Grid from '@material-ui/core/Grid';
+import {DataGrid} from '@material-ui/data-grid';
+import Button from '@material-ui/core/Button';
 import Cookies from 'js-cookie';
-import {SERVER_URL} from '../constants.js';
 
-// NOTE:  for OAuth security, http request must have
-//   credentials: 'include' 
-//
 
-//  required properties -  assignment
-//  
-//  NOTE: because Gradebook is invoked via <Link> in Assignment.js components
-//  properties are passed as attributes of props.location 
-//
-class Gradebook extends React.Component {
+//  Gradebook 
+//    properties -  assignment:  
+
+class Gradebook extends Component {
     constructor(props) {
       super(props);
-      console.log("Gradebook.cnstr "+ JSON.stringify(props.location));
-      this.state = {  grades :  [] };
+      console.log("Gradebook cnstr "+ JSON.stringify(props.location.assignment));
+      this.state = { rows :  [  ]};
     } 
     
      componentDidMount() {
@@ -28,21 +23,19 @@ class Gradebook extends React.Component {
     }
  
     fetchGrades = () => {
-      console.log("Gradebook.fetchGrades");
+      console.log("FETCH");
       const token = Cookies.get('XSRF-TOKEN');
-      fetch(`${SERVER_URL}/gradebook/${this.props.location.assignment.assignmentId}`, 
+      fetch(SERVER_URL + '/gradebook/' + this.props.location.assignment.assignmentId , 
         {  
           method: 'GET', 
-          headers: { 'X-XSRF-TOKEN': token }
+          headers: { 'X-XSRF-TOKEN': token }, 
+          credentials: 'include'
         } )
       .then((response) => response.json()) 
       .then((responseData) => { 
         if (Array.isArray(responseData.grades)) {
-          // add attribute "id" to each row. Required for DataGrid,  id is index of row (i.e. 0, 1, 2, ...)  
           this.setState({ 
-            grades: responseData.grades.map((r,index) => {
-                  return {id:index, ...r};
-            })
+            rows: responseData.grades,
           });
         } else {
           toast.error("Fetch failed.", {
@@ -50,26 +43,21 @@ class Gradebook extends React.Component {
           });
         }        
       })
-      .catch(err => {
-        toast.error("Fetch failed.", {
-            position: toast.POSITION.BOTTOM_LEFT
-          });
-          console.error(err); 
-      })
+      .catch(err => console.error(err)); 
     }
   
-    // when submit button pressed, send updated grades to back end 
-    //  and then fetch the new grades.
-    handleSubmit = ( ) => {
-      console.log("Gradebook.handleSubmit");
+   // when submit button pressed, send updated grades to back end 
+   //  and then fetch the new grades.
+   handleSubmit = ( ) => {
+      console.log("PUT");
       const token = Cookies.get('XSRF-TOKEN');
-      
-      fetch(`${SERVER_URL}/gradebook/${this.props.location.assignment.assignmentId}` , 
+      fetch(SERVER_URL + '/gradebook/' + this.props.location.assignment.assignmentId , 
           {  
             method: 'PUT', 
             headers: { 'Content-Type': 'application/json',
                        'X-XSRF-TOKEN': token }, 
-            body: JSON.stringify({assignmentId:this.props.location.assignment.assignmentId,  grades: this.state.grades})
+            credentials: 'include',
+            body: JSON.stringify({assignmentId:this.props.location.assignment.assignmentId,  grades: this.state.rows})
           } )
       .then(res => {
           if (res.ok) {
@@ -92,58 +80,49 @@ class Gradebook extends React.Component {
    };        
     
     // when user has entered a new grade, update the state
-    //  id    - index of row of grade change
-    //  props - contains the new grade
     handleEditCellChange = ({ id, field, props }) => {
-       console.log("edit cell change id:"+id+" field:"+field+" value:"+props.value);
-       const newgrades = this.state.grades.map(r => {
-         if (r.id === id){
-           return {...r, [field]:props.value};
-         } else {
-           return {...r};
-         }
-       });
-       this.setState({grades: newgrades});
-     };
 
-    handleCellEditCommit = (e) => {
-      console.log("handleCellEditCommit "+JSON.stringify(e));
-      const newgrades= this.state.grades.map(r => {
-        //console.log(r.id+"   "+e.id);
-        if (r.id === e.id) {
-          return {...r, [e.field]:e.value};
-        } else {
-          return {...r};
-        }
+      // updating state when state is an array
+      this.setState(state => {
+        const rows = state.rows.map((item, j) => {
+          if (j === id) {
+            return {...item, grade:props.value}
+          } else {
+            return item;
+          }
+        });
+        const t = { rows, };
+        console.log("edit cell end. "+JSON.stringify(t));
+        return t;
       });
-      this.setState({grades: newgrades});
-    };
+    };    
  
     render() {
        const columns = [
+        { field: 'assignmentGradeId', headerName: 'Key', width: 150 },
         { field: 'name', headerName: 'Name', width: 250 },
         { field: 'email', headerName: 'Email', width: 250},
         { field: 'grade', headerName: 'Grade', width: 150 , editable:true}
         ];
-        
-        const assignment = this.props.location.assignment;
       
         return(
             <div className="App">
               <Grid container>
-                <Grid item align="left">
-                   <h4>Assignment: {assignment.assignmentName}</h4>
-                   <h4>Course: {assignment.courseTitle}</h4>                   
+                <Grid item>
+                    <h4>{this.props.location.assignment.assignmentName}-
+                        {this.props.location.assignment.courseTitle}</h4>                   
                 </Grid>
               </Grid>
-              <div style={{width:'100%'}}>
-                For DEBUG:  display state.
-                {JSON.stringify(this.state)}
-              </div>
               <div style={{ height: 400, width: '100%' }}>
-                <DataGrid rows={this.state.grades} columns={columns} onCellEditCommit={this.handleCellEditCommit}  />
-                <Button id="Submit" variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleSubmit} >
-                   Submit
+                <DataGrid 
+                  rows={this.state.rows.map((row,index) => {
+                          return {id:index, assignmentGradeId: row.assignmentGradeId, name: row.name, email: row.email, grade: row.grade};
+                          } )
+                       } 
+                  columns={columns}                   
+                  onEditCellChange={this.handleEditCellChange}  />
+                <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleSubmit} >
+                  Submit
                 </Button>
               </div>
               <ToastContainer autoClose={1500} />   
